17ffcc73a4a43b1f687298416564d0bde3375535
unknown <pranjwalsingh236@gmail.com>
2022-02-03 21:59:37 +0530

    Added assignment

---
+++ basic_syntax_conditions_loops/que1.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cout << "Enter a number: ";
+    cin >> x;
+    cout << "The number entered is: " << x;
+    return 0;
+}
