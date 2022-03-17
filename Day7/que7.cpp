95ffb84c54433ce4c7a408a8f8519eb7c629df00
Pranjwal Singh <66786315+pranjwalsingh236@users.noreply.github.com>
2022-03-17 15:13:20 +0530

    Add files via upload

---
+++ Day7/que7.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+public:
+    void helper(vector<int> &nums, vector<vector<int>> &ans, vector<int> subset, int idx)
+    {
+        ans.push_back(subset);
+        if (idx == nums.size())
+        {
+            return;
+        }
+        subset.push_back(nums[idx]);
+        helper(nums, ans, subset, idx + 1);
+        subset.pop_back();
+        helper(nums, ans, subset, idx + 1);
+    }
+    vector<vector<int>> subsets(vector<int> &nums)
+    {
+        vector<int> subset;
+        vector<vector<int>> ans;
+        helper(nums, ans, subset, 0);
+        set<vector<int>> st;
+        for (auto i : ans)
+        {
+            st.insert(i);
+        }
+        ans.clear();
+        for (auto i : st)
+        {
+            ans.push_back(i);
+        }
+        return ans;
+    }
+};
