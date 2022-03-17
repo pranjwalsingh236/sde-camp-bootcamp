b493475205efa2de3353482cede309602dc99814
Pranjwal Singh <66786315+pranjwalsingh236@users.noreply.github.com>
2022-03-17 15:33:41 +0530

    Update que7.cpp

--- Day7/que7.cpp
+++ Day7/que7.cpp
@@ -3,9 +3,10 @@ class Solution
 public:
     void helper(vector<int> &nums, vector<vector<int>> &ans, vector<int> subset, int idx)
     {
-        ans.push_back(subset);
+
         if (idx == nums.size())
         {
+            ans.push_back(subset);
             return;
         }
         subset.push_back(nums[idx]);
@@ -18,16 +19,6 @@ public:
         vector<int> subset;
         vector<vector<int>> ans;
         helper(nums, ans, subset, 0);
-        set<vector<int>> st;
-        for (auto i : ans)
-        {
-            st.insert(i);
-        }
-        ans.clear();
-        for (auto i : st)
-        {
-            ans.push_back(i);
-        }
         return ans;
     }
 };
